0e40731a9184b30afa34a86ef6561c16cb089f18
Chaz Beaver <username@email.com>
2025-06-04 12:53:58 -0400

    add mac alias to edit starship.toml

--- active/shared/HOME/zsh/zsh_modules/macos/alias.sh
+++ active/shared/HOME/zsh/zsh_modules/macos/alias.sh
@@ -1,7 +1,11 @@
 # For Mac
 alias here='open .'
 
-# Edit Config Files
+# Edit Ghostty Config
 edit-ghostty() {
     vim $HOME/Library/Application\ Support/com.mitchellh.ghostty/config
+}
+# Edit Starship Config File
+edit-starship() {
+    vim $HOME/.config/starship.toml
 }
