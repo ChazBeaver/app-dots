5fe9b2e66977b09be0df0f3a6d67ba5495392ade
User Name <username@email.com>
2025-06-19 16:04:00 -0400

    add new themes

--- active/shared/.config/nvim/lua/themes/init.lua
+++ active/shared/.config/nvim/lua/themes/init.lua
@@ -5,7 +5,7 @@ local loader = require("themes.loader")
 local apply = require("themes.apply")
 
 -- Load default theme on startup
-local default_name = "realtree-edge"
+local default_name = "dark-lotus"
 local colors = loader.parse(default_name)
 apply.apply(colors)
 vim.g.colors_name = default_name
