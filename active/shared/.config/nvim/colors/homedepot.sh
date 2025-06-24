ae66deb4e131b95a605f9e00774c9479fbdc99ba
Chaz Beaver <username@email.com>
2025-06-24 16:18:10 -0400

    update homedepot theme

---
+++ active/shared/.config/nvim/colors/homedepot.sh
@@ -0,0 +1,30 @@
+# ~/.config/nvim/colors/homedepot.sh
+
+color01=#FF9542  # keyword (burnt orange)
+color02=#d45a00  # string (power tool orange)
+color03=#ffffff  # number (highlighted tag)
+color04=#9D5E25  # function (contractor copper)
+color05=#aaaaaa  # identifier (tempered gray)
+color06=#ffb871  # type (soft sanded orange)
+
+color07=#121212  # code block background
+color08=#3c3c3c  # heading 6 / grey label
+color09=#7c7c7c  # comments (medium gray)
+color10=#000000  # background (flat black)
+color11=#ff4f4f  # diagnostic error (caution red)
+color12=#d6a000  # diagnostic warn (workzone amber)
+
+color13=#161616  # cursorline (matte black)
+color14=#e6e6e6  # foreground / text (bright white)
+color15=#2a2a2a  # dim inactive (shelved)
+color16=#343434  # visual selection (highlight shadow)
+color17=#181818  # statusline active (steel tone)
+color18=#2e2e2e  # heading alt 1
+color19=#393939  # heading alt 2
+color20=#464646  # heading alt 3
+color21=#515151  # heading alt 4
+color22=#5c5c5c  # heading alt 5
+color23=#676767  # heading alt 6
+color24=#ffffff  # cursor (white highlight)
+color25=#1c1c1c  # statusline inactive
+color26=#0d0d0d  # heading foreground
