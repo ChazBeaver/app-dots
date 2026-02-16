a418620f67debac97566b80e679bdeb5bdceaec8
Chaz Beaver <HyprCore@Arch.com>
2026-02-16 12:21:11 -0500

    remove autocomplete; install autosuggestions

--- active/shared/HOME/zsh/zsh_modules/linux/plugins.sh
+++ active/shared/HOME/zsh/zsh_modules/linux/plugins.sh
@@ -9,22 +9,22 @@
 # -----------------------------
 # zsh-autosuggestions
 # -----------------------------
-# ZSH_AUTOSUGGESTIONS_FILE="/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
-#
-# if [[ -r "$ZSH_AUTOSUGGESTIONS_FILE" ]]; then
-#   source "$ZSH_AUTOSUGGESTIONS_FILE"
-#
-#   # nicer defaults
-#   ZSH_AUTOSUGGEST_STRATEGY=(history completion)
-#   ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#666666'
-# fi
+ZSH_AUTOSUGGESTIONS_FILE="/usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh"
 
+if [[ -r "$ZSH_AUTOSUGGESTIONS_FILE" ]]; then
+  source "$ZSH_AUTOSUGGESTIONS_FILE"
 
-# -----------------------------
-# zsh-autocomplete
-# -----------------------------
-ZSH_AUTOCOMPLETE_FILE="/usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
-
-if [[ -r "$ZSH_AUTOCOMPLETE_FILE" ]]; then
-  source "$ZSH_AUTOCOMPLETE_FILE"
+  # nicer defaults
+  ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+  ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#666666'
 fi
+
+
+# # -----------------------------
+# # zsh-autocomplete
+# # -----------------------------
+# ZSH_AUTOCOMPLETE_FILE="/usr/share/zsh/plugins/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
+#
+# if [[ -r "$ZSH_AUTOCOMPLETE_FILE" ]]; then
+#   source "$ZSH_AUTOCOMPLETE_FILE"
+# fi
