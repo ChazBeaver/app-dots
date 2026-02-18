363a2a2ad8d2ff167fe37073cf1215be96952c79
Chaz Beaver <HyprCore@Arch.com>
2026-02-18 09:53:08 -0500

    fix treesitter version/path issue

--- active/linux/.config/nvim/lua/plugins/treesitter.lua
+++ active/linux/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,33 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
+  {
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            ensure_installed = {"lua", "bash", "c", "javascript", "go"},
-            highlight = { enable = true },
-            indent = { enable = true }
-        })
-    end
+      require("nvim-treesitter.config").setup({
+        ensure_installed = { "lua", "bash", "c", "javascript", "go" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }
+
+-- -- BULLETPROOF VERSION --
+-- return {
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = ":TSUpdate",
+--     config = function()
+--       local ok, ts = pcall(require, "nvim-treesitter.configs")
+--       if not ok then
+--         ts = require("nvim-treesitter.config")
+--       end
+--
+--       ts.setup({
+--         ensure_installed = { "lua", "bash", "c", "javascript", "go" },
+--         highlight = { enable = true },
+--         indent = { enable = true },
+--       })
+--     end,
+--   },
+-- }
