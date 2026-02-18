549878f61a495ad20da4ad037b53e16bf12b947f
User Name <username@email.com>
2026-02-18 10:16:56 -0500

    update treesitter to 'bulletproof' version

--- active/linux/.config/nvim/lua/plugins/treesitter.lua
+++ active/linux/.config/nvim/lua/plugins/treesitter.lua
@@ -1,29 +1,9 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.config").setup({
-        ensure_installed = { "lua", "bash", "c", "javascript", "go" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
-}
-
--- -- BULLETPROOF VERSION --
 -- return {
 --   {
 --     "nvim-treesitter/nvim-treesitter",
 --     build = ":TSUpdate",
 --     config = function()
---       local ok, ts = pcall(require, "nvim-treesitter.configs")
---       if not ok then
---         ts = require("nvim-treesitter.config")
---       end
---
---       ts.setup({
+--       require("nvim-treesitter.config").setup({
 --         ensure_installed = { "lua", "bash", "c", "javascript", "go" },
 --         highlight = { enable = true },
 --         indent = { enable = true },
@@ -31,3 +11,23 @@ return {
 --     end,
 --   },
 -- }
+
+-- BULLETPROOF VERSION --
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local ok, ts = pcall(require, "nvim-treesitter.configs")
+      if not ok then
+        ts = require("nvim-treesitter.config")
+      end
+
+      ts.setup({
+        ensure_installed = { "lua", "bash", "c", "javascript", "go" },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+}
