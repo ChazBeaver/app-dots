343f60c4eeb20b5705c140f0e215338e23dd52aa
Chaz Beaver <chazbeaver@gmail.com>
2026-03-02 13:50:24 -0500

    update file path display to top/center in neovim

--- active/macos/.config/nvim/init.lua
+++ active/macos/.config/nvim/init.lua
@@ -1,7 +1,8 @@
 -- Map <leader>
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = " "
- 
+vim.opt.winbar = "%=%f%="
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
