648c2ba8c0dab6a0a37368849bd6b4f446274e38
chazbeaver <chazbeaver@gmail.com>
2026-07-02 14:25:47 -0400

    comment out function; no longer used

--- active/shared/HOME/zsh/zsh_modules/macos/functions.sh
+++ active/shared/HOME/zsh/zsh_modules/macos/functions.sh
@@ -1,9 +1,9 @@
-# Select Theme for Zsh, Neovim, Kitty, Starship, and Btop
-tt() {
-  local script="$APP_DOTS_DIR/active/macos/.config/colors/pick-theme.sh"
-  if [[ -x "$script" ]]; then
-    bash "$script"
-  else
-    echo "❌ pick-theme.sh not found or not executable at: $script"
-  fi
-}
+# # Select Theme for Zsh, Neovim, Kitty, Starship, and Btop
+# tt() {
+#   local script="$APP_DOTS_DIR/active/macos/.config/colors/pick-theme.sh"
+#   if [[ -x "$script" ]]; then
+#     bash "$script"
+#   else
+#     echo "❌ pick-theme.sh not found or not executable at: $script"
+#   fi
+# }
