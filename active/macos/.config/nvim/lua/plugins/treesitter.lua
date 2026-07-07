a80b54a024e93fbf1bf42c4529352a6a4099fb20
Chaz Beaver <HyprCore@Arch.com>
2026-07-07 12:50:30 -0400

    update treesitter on Mac

--- active/macos/.config/nvim/lua/plugins/treesitter.lua
+++ active/macos/.config/nvim/lua/plugins/treesitter.lua
@@ -1,14 +1,31 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        ensure_installed = { "lua", "bash", "c", "javascript", "go" },
-        highlight = { enable = true },
-        indent = { enable = true },
+      require("nvim-treesitter").setup()
+
+      -- Parsers you want installed
+      local ensure_installed = { "lua", "bash", "c", "javascript", "go" }
+
+      local installed = require("nvim-treesitter.config").get_installed()
+      local missing = vim.tbl_filter(function(lang)
+        return not vim.tbl_contains(installed, lang)
+      end, ensure_installed)
+      if #missing > 0 then
+        require("nvim-treesitter").install(missing)
+      end
+
+      -- Enable highlighting + indentation per buffer
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          if pcall(vim.treesitter.start, ev.buf) then
+            vim.bo[ev.buf].indentexpr =
+              "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
       })
     end,
   },
